462f833acb0e31a9c38a14cb2a0ca519101c5aec
Oleh Solomka <olehsolomka@gmail.com>
2022-02-08 11:06:49 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
 // import Loader from "./components/Loader/Loader";
-import { useState } from "react";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import Searchbar from "./components/Searchbar";
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import { useState, useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+import Searchbar from "./components/Searchbar";
 import ImageGallery from "./components/ImageGallery";
+import imageAPI from "./components/services/image-api";
+import Loader from "./components/Loader";
+import Button from "./components/Button";
+
+const Status = {
+  IDLE: "idle",
+  PENDING: "pending",
+  RESOLVED: "resolved",
+  REJECTED: "rejected",
+};
 
 const App = () => {
   const [targetImg, setTargetImg] = useState("");
+  const [collection, setCollection] = useState([]);
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState(Status.IDLE);
+
+  useEffect(() => {
+    if (!targetImg) {
+      return;
+    }
+    setStatus(Status.PENDING);
+
+    imageAPI
+      .fetchImage(targetImg)
+      .then(({ hits }) => {
+        setCollection(hits);
+        setStatus(Status.RESOLVED);
+      })
+      .catch((error) => {
+        setError("ERROR");
+        setStatus(Status.REJECTED);
+        setPage(1);
+      });
+  }, [targetImg]);
+
+  useEffect(() => {
+    if (!targetImg) {
+      return;
+    }
+    imageAPI
+      .fetchImage(targetImg, page)
+      .then(({ hits }) => {
+        setCollection((prevState) => [...prevState, ...hits]);
+        setStatus(Status.RESOLVED);
+      })
+      .catch((error) => {
+        setError("ERROR");
+        setStatus(Status.REJECTED);
+        setPage(1);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [page]);
+
+  const onLoadMoreButton = () => {
+    setPage((prevState) => prevState + 1);
+  };
 
   return (
     <div className="app">
@@ -18,7 +73,13 @@ const App = () => {
         draggable={false}
         pauseOnHover={false}
       />
-      <ImageGallery searchImg={targetImg} />
+      {status === Status.IDLE && <></>}
+      {status === Status.PENDING && <Loader />}
+      {status === Status.REJECTED && <h1>{error}</h1>}
+      {status === Status.RESOLVED && (
+        <ImageGallery imageCollection={collection} />
+      )}
+      {collection.length >= 20 && <Button onClick={onLoadMoreButton} />}
     </div>
   );
 };
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,101 +1,34 @@
-import { useState, useEffect } from "react";
 import s from "./ImageGallery.module.css";
+import { useState } from "react";
 import ImageGalleryItem from "../ImageGalleryItem";
-import Loader from "../Loader";
-import imageAPI from "../services/image-api";
-import Button from "../Button";
 import Modal from "../Modal";
 
-const Status = {
-  IDLE: "idle",
-  PENDING: "pending",
-  RESOLVED: "resolved",
-  REJECTED: "rejected",
-};
-
-export default function ImageGallery({ searchImg }) {
-  const [collection, setCollection] = useState(null);
-  const [page, setPage] = useState(1);
-  const [status, setStatus] = useState(Status.IDLE);
-  const [error, setError] = useState(null);
+export default function ImageGallery({ imageCollection }) {
   const [showModal, setShowModal] = useState(false);
   const [activeTabIxd, setActiveTabIxd] = useState(0);
 
-  useEffect(() => {
-    if (!searchImg) {
-      return;
-    }
-
-    setStatus(Status.PENDING);
-
-    imageAPI
-      .fetchImage(searchImg)
-      .then(({ hits }) => {
-        setCollection(hits);
-        setStatus(Status.RESOLVED);
-      })
-      .catch((error) => {
-        setError(error);
-        setPage(1);
-      });
-  }, [searchImg]);
-
-  useEffect(() => {
-    if (!searchImg) {
-      return;
-    }
-
-    imageAPI
-      .fetchImage(searchImg, page)
-      .then(({ hits }) => setCollection((prevState) => [...prevState, ...hits]))
-      .catch((error) => {
-        setError(error);
-        setStatus(Status.REJECTED);
-        setPage(1);
-      });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page]);
-
-  const onLoadMoreButton = () => {
-    setPage((prevState) => prevState + 1);
-  };
   const toggleModal = () => {
     setShowModal((prevState) => !prevState);
   };
 
-  if (status === Status.IDLE) {
-    return <></>;
-  }
-  if (status === Status.PENDING) {
-    return <Loader />;
-  }
-  if (status === Status.REJECTED) {
-    return <h1>{error.message}</h1>;
-  }
-  if (status === Status.RESOLVED) {
-    if (collection.length === 0) {
-      return <h1 className={s.alert}>No Images found</h1>;
-    }
-    const activeImg = collection[activeTabIxd];
-    return (
-      <>
-        {showModal && (
-          <Modal src={activeImg.webformatURL} onModalToggle={toggleModal} />
-        )}
-        <ul className={s.imageGallery}>
-          {collection.map((image, index) => (
-            <ImageGalleryItem
-              key={index}
-              image={image.webformatURL}
-              tag={image.tags}
-              onClick={setActiveTabIxd}
-              onModalToggle={toggleModal}
-              index={index}
-            />
-          ))}
-        </ul>
-        {collection.length >= 20 && <Button onClick={onLoadMoreButton} />}
-      </>
-    );
-  }
+  const activeImg = imageCollection[activeTabIxd];
+  return (
+    <>
+      {showModal && (
+        <Modal src={activeImg.webformatURL} onModalToggle={toggleModal} />
+      )}
+      <ul className={s.imageGallery}>
+        {imageCollection.map((image, index) => (
+          <ImageGalleryItem
+            key={index}
+            image={image.webformatURL}
+            tag={image.tags}
+            onClick={setActiveTabIxd}
+            onModalToggle={toggleModal}
+            index={index}
+          />
+        ))}
+      </ul>
+    </>
+  );
 }
